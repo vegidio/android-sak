52a36950fe95fe0041c8047f2dc4d6fb9ffb4637
Vinicius Egidio <me@vinicius.io>
2024-01-16 10:10:11 +0100

    👷🏾‍♂️ Reactivated the CI pipeline for Android-SAK

--- publish.gradle.kts
+++ publish.gradle.kts
@@ -1,6 +1,18 @@
 configure<PublishingExtension> {
     publications {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/vegidio/android-sak")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
         create<MavenPublication>("maven") {
+            groupId = "io.vinicius.sak"
+            version = System.getenv("VERSION")
             from(components["release"])
         }
     }
